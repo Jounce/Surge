b972f44a1e374b36622a596e3e64df20fa19c676
Mattt Thompson <m@mattt.me>
2014-09-23 09:42:52 -0700

    Adjusting performance benchmarks

--- Tests/SurgeTests.swift
+++ Tests/SurgeTests.swift
@@ -3,7 +3,7 @@ import Surge
 import XCTest
 
 class SurgeTests: XCTestCase {
-    let count: Int = 100000000
+    let count: Int = 100000
     var repeatedValue: Double = Double(arc4random())
     var numbers: [Double] = []
 
@@ -11,15 +11,15 @@ class SurgeTests: XCTestCase {
         self.numbers = [Double](count: count, repeatedValue: repeatedValue)
     }
 
-//    func testPerformanceNaive() {
-//        var sum: Double = 0.0
-//
-//        self.measureBlock() {
-//            sum = reduce(self.numbers, 0.0, +)
-//        }
-//
-//        XCTAssertEqualWithAccuracy(sum, repeatedValue * Double(count), 1.0, "incorrect sum")
-//    }
+    func testPerformanceNaive() {
+        var sum: Double = 0.0
+
+        self.measureBlock() {
+            sum = reduce(self.numbers, 0.0, +)
+        }
+
+        XCTAssertEqualWithAccuracy(sum, repeatedValue * Double(count), 1.0, "incorrect sum")
+    }
 
     func testPerformanceSurge() {
         var sum: Double = 0.0
