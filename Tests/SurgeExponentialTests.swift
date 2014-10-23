0dfdcc0f418aa8247f8b0fade354eaecd4b7d22c
UsrNameu1 <tea_room_1028@yahoo.co.jp>
2014-10-24 01:56:34 +0900

    Add Exponential Tests

---
+++ Tests/SurgeExponentialTests.swift
@@ -0,0 +1,38 @@
+//
+//  SurgeExponentialTests.swift
+//  Surge
+//
+//  Created by adachi yuichi on 2014/10/24.
+//  Copyright (c) 2014年 Mattt Thompson. All rights reserved.
+//
+
+import XCTest
+import Surge
+
+class SurgeExponentialTests: XCTestCase {
+
+    func testExp() {
+        var elist = [Double]()
+        var etoTwo = exp(2.0)
+        var etoThree = exp(3.0)
+        
+        self.measureBlock() {
+            elist = exp([0.0, 0.0], [2.0, 3.0])
+        }
+        
+        XCTAssertEqual(elist, [etoTwo, etoThree], "incollect exp")
+    }
+    
+    func testExp2() {
+        var twolist = [Double]()
+        var twoToTwelve = exp2(12.0)
+        var twoToNine = exp2(9.0)
+        var twoToFive = exp2(5.0)
+        
+        self.measureBlock() {
+            twolist = exp2([0.0, 0.0, 0.0], [12.0, 9.0, 5.0])
+        }
+        
+        XCTAssertEqual(twolist, [twoToTwelve, twoToNine, twoToFive], "incollect exp2")
+    }
+}
