ebf465987ab49718887d0da0740cefdef1d9c7b7
Mattt Thompson <m@mattt.me>
2014-11-29 17:11:01 -0800

    Refactoring and reformatting

--- Source/Matrix.swift
+++ Source/Matrix.swift
@@ -1,51 +1,77 @@
-import Foundation
-import Accelerate
+// Hyperbolic.swift
+//
+// Copyright (c) 2014 Mattt Thompson (http://mattt.me)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 public struct Matrix {
-    let rows: Int, columns: Int
+    let rows: Int
+    let columns: Int
     var grid: [Double]
     
-    public init(rows: Int, columns: Int, value: Double) {
+    public init(rows: Int, columns: Int, repeatedValue: Double) {
         self.rows = rows
         self.columns = columns
-        grid = Array(count: rows * columns, repeatedValue: value)
-    }
-    
-    public init(rows: Int, columns: Int) {
-        self.init(rows: rows, columns: columns, value: 0.0)
+
+        self.grid = [Double](count: rows * columns, repeatedValue: repeatedValue)
     }
     
     public init(_ rows: [Double]...) {
-        let nRows: Int = rows.count
-        let nColumns: Int = rows[0].count
-        self.init(rows: nRows, columns: nColumns, value: 0.0)
+        let m: Int = rows.count
+        let n: Int = rows[0].count
+        self.init(rows: m, columns: n, repeatedValue: 0.0)
+
         for (i, row) in enumerate(rows) {
-            let nValues = min(nColumns, row.count)
-            grid.replaceRange(i*nColumns..<i*nColumns+nValues, with: row)
+            grid.replaceRange(i*n..<i*n+min(m, row.count), with: row)
         }
-    }
-    
-    func indexIsValidForRow(row: Int, column: Int) -> Bool {
-        return row >= 0 && row < rows && column >= 0 && column < columns
     }
     
     public subscript(row: Int, column: Int) -> Double {
         get {
-            assert(indexIsValidForRow(row, column: column), "Index out of range")
+            assert(indexIsValidForRow(row, column: column))
             return grid[(row * columns) + column]
         }
+
         set {
-            assert(indexIsValidForRow(row, column: column), "Index out of range")
+            assert(indexIsValidForRow(row, column: column))
             grid[(row * columns) + column] = newValue
         }
     }
-    
-    
-    
+
+    private func indexIsValidForRow(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }    
 }
 
+// MARK: - ArrayLiteralConvertible
+
+extension Matrix: ArrayLiteralConvertible {
+    public init(arrayLiteral elements: Double...) {
+        self.init(elements)
+    }
+}
+
+// MARK: -
+
 public func add(x: Matrix, y: Matrix) -> Matrix {
-    assert(x.rows == y.rows && x.columns == y.columns, "Matrix dimensions not compatible with addition")
+    precondition(x.rows == y.rows && x.columns == y.columns, "Matrix dimensions not compatible with addition")
+
     var results = y
     cblas_daxpy(Int32(x.grid.count), 1.0, x.grid, 1, &(results.grid), 1)
     
@@ -60,34 +86,36 @@ public func mul(alpha: Double, x:Matrix) -> Matrix {
 }
 
 public func mul(x: Matrix, y: Matrix) -> Matrix {
-    assert(x.columns == y.rows, "Matrix dimensions not compatible with multiplication")
-    var results = Matrix(rows: x.rows, columns: y.columns)
+    precondition(x.columns == y.rows, "Matrix dimensions not compatible with multiplication")
+
+    var results = Matrix(rows: x.rows, columns: y.columns, repeatedValue: 0.0)
     cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, Int32(x.rows), Int32(y.columns), Int32(x.columns), 1.0, x.grid, Int32(x.columns), y.grid, Int32(y.columns), 0.0, &(results.grid), Int32(results.columns))
     
     return results
 }
 
 public func inv(x : Matrix) -> Matrix {
-    assert(x.rows == x.columns, "To take an inverse of a matrix, the matrix must be square.")
+    precondition(x.rows == x.columns, "Matrix must be square")
+
     var results = x
     
-    var ipiv:Array<__CLPK_integer> = Array(count:x.rows*x.rows, repeatedValue:0)
-    var lwork:__CLPK_integer = __CLPK_integer(x.columns*x.columns)
+    var ipiv = [__CLPK_integer](count: x.rows * x.rows, repeatedValue: 0)
+    var lwork = __CLPK_integer(x.columns * x.columns)
     var work = [CDouble](count: Int(lwork), repeatedValue: 0.0)
-    var error:__CLPK_integer=0
+    var error: __CLPK_integer = 0
     var nc = __CLPK_integer(x.columns)
+
     dgetrf_(&nc, &nc, &(results.grid), &nc, &ipiv, &error)
     dgetri_(&nc, &(results.grid), &nc, &ipiv, &work, &lwork, &error)
+
     assert(error == 0, "Matrix not invertible")
     
     return results
-    
 }
 
 public func transpose(x: Matrix) -> Matrix {
-    var results = Matrix(rows: x.columns, columns: x.rows)
+    var results = Matrix(rows: x.columns, columns: x.rows, repeatedValue: 0.0)
     vDSP_mtransD(x.grid, 1, &(results.grid), 1, vDSP_Length(results.rows), vDSP_Length(results.columns))
     
     return results
 }
-
