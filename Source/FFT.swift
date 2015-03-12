3e8c754587aa058a6a317406268a48a4980c727c
Mattt Thompson <m@mattt.me>
2015-03-12 13:07:32 -0700

    [Issue #12] Fixing memory leak in FFT function

--- Source/FFT.swift
+++ Source/FFT.swift
@@ -40,6 +40,8 @@ public func fft(input: [Float]) -> [Float] {
     var normalizedMagnitudes = [Float](count: input.count, repeatedValue: 0.0)
     vDSP_vsmul(sqrt(magnitudes), 1, [2.0 / Float(input.count)], &normalizedMagnitudes, 1, vDSP_Length(input.count))
 
+    vDSP_destroy_fftsetup(weights)
+
     return normalizedMagnitudes
 }
 
@@ -58,6 +60,8 @@ public func fft(input: [Double]) -> [Double] {
 
     var normalizedMagnitudes = [Double](count: input.count, repeatedValue: 0.0)
     vDSP_vsmulD(sqrt(magnitudes), 1, [2.0 / Double(input.count)], &normalizedMagnitudes, 1, vDSP_Length(input.count))
+
+    vDSP_destroy_fftsetupD(weights)
 
     return normalizedMagnitudes
 }
