e24c0da2b2fadc961720fcc8751beee59ea1a722
Alejandro Isaza <aisaza@venturemedia.com>
2015-09-25 17:19:49 -0700

    Make overloaded operators public

--- Source/Arithmetic.swift
+++ Source/Arithmetic.swift
@@ -277,91 +277,91 @@ public func dot(x: [Double], _ y: [Double]) -> Double {
 
 // MARK: - Operators
 
-func + (lhs: [Float], rhs: [Float]) -> [Float] {
+public func + (lhs: [Float], rhs: [Float]) -> [Float] {
     return add(lhs, rhs)
 }
 
-func + (lhs: [Double], rhs: [Double]) -> [Double] {
+public func + (lhs: [Double], rhs: [Double]) -> [Double] {
     return add(lhs, rhs)
 }
 
-func + (lhs: [Complex], rhs: [Complex]) -> [Complex] {
+public func + (lhs: [Complex], rhs: [Complex]) -> [Complex] {
     return add(lhs, rhs)
 }
 
-func + (lhs: [Float], rhs: Float) -> [Float] {
+public func + (lhs: [Float], rhs: Float) -> [Float] {
     return add(lhs, [Float](count: lhs.count, repeatedValue: rhs))
 }
 
-func + (lhs: [Double], rhs: Double) -> [Double] {
+public func + (lhs: [Double], rhs: Double) -> [Double] {
     return add(lhs, [Double](count: lhs.count, repeatedValue: rhs))
 }
 
-func + (lhs: [Complex], rhs: Complex) -> [Complex] {
+public func + (lhs: [Complex], rhs: Complex) -> [Complex] {
     return add(lhs, [Complex](count: lhs.count, repeatedValue: rhs))
 }
 
-func / (lhs: [Float], rhs: [Float]) -> [Float] {
+public func / (lhs: [Float], rhs: [Float]) -> [Float] {
     return div(lhs, rhs)
 }
 
-func / (lhs: [Double], rhs: [Double]) -> [Double] {
+public func / (lhs: [Double], rhs: [Double]) -> [Double] {
     return div(lhs, rhs)
 }
 
-func / (lhs: [Float], rhs: Float) -> [Float] {
+public func / (lhs: [Float], rhs: Float) -> [Float] {
     return div(lhs, [Float](count: lhs.count, repeatedValue: rhs))
 }
 
-func / (lhs: [Double], rhs: Double) -> [Double] {
+public func / (lhs: [Double], rhs: Double) -> [Double] {
     return div(lhs, [Double](count: lhs.count, repeatedValue: rhs))
 }
 
-func / (lhs: [Complex], rhs: Double) -> [Complex] {
+public func / (lhs: [Complex], rhs: Double) -> [Complex] {
     return div(lhs, [Double](count: lhs.count, repeatedValue: rhs))
 }
 
-func * (lhs: [Float], rhs: [Float]) -> [Float] {
+public func * (lhs: [Float], rhs: [Float]) -> [Float] {
     return mul(lhs, rhs)
 }
 
-func * (lhs: [Double], rhs: [Double]) -> [Double] {
+public func * (lhs: [Double], rhs: [Double]) -> [Double] {
     return mul(lhs, rhs)
 }
 
-func * (lhs: [Float], rhs: Float) -> [Float] {
+public func * (lhs: [Float], rhs: Float) -> [Float] {
     return mul(lhs, [Float](count: lhs.count, repeatedValue: rhs))
 }
 
-func * (lhs: [Double], rhs: Double) -> [Double] {
+public func * (lhs: [Double], rhs: Double) -> [Double] {
     return mul(lhs, [Double](count: lhs.count, repeatedValue: rhs))
 }
 
-func * (lhs: [Complex], rhs: Double) -> [Complex] {
+public func * (lhs: [Complex], rhs: Double) -> [Complex] {
     return mul(lhs, [Double](count: lhs.count, repeatedValue: rhs))
 }
 
-func % (lhs: [Float], rhs: [Float]) -> [Float] {
+public func % (lhs: [Float], rhs: [Float]) -> [Float] {
     return mod(lhs, rhs)
 }
 
-func % (lhs: [Double], rhs: [Double]) -> [Double] {
+public func % (lhs: [Double], rhs: [Double]) -> [Double] {
     return mod(lhs, rhs)
 }
 
-func % (lhs: [Float], rhs: Float) -> [Float] {
+public func % (lhs: [Float], rhs: Float) -> [Float] {
     return mod(lhs, [Float](count: lhs.count, repeatedValue: rhs))
 }
 
-func % (lhs: [Double], rhs: Double) -> [Double] {
+public func % (lhs: [Double], rhs: Double) -> [Double] {
     return mod(lhs, [Double](count: lhs.count, repeatedValue: rhs))
 }
 
 infix operator • {}
-func • (lhs: [Double], rhs: [Double]) -> Double {
+public func • (lhs: [Double], rhs: [Double]) -> Double {
     return dot(lhs, rhs)
 }
 
-func • (lhs: [Float], rhs: [Float]) -> Float {
+public func • (lhs: [Float], rhs: [Float]) -> Float {
     return dot(lhs, rhs)
 }
