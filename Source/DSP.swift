2553d40b5208d184900706f4e6dac3bdead26618
Alejandro Isaza <aisaza@venturemedia.com>
2015-09-30 12:11:05 -0700

    Add DSP functions
    Includes convolution, correlation and autocorrelation

---
+++ Source/DSP.swift
@@ -0,0 +1,60 @@
+//  Copyright © 2015 Alejandro Isaza. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import Foundation
+
+/**
+    Convolution between a signal and a kernel. The signal should have at least as many elements as the kernel. The
+    result will have `N - M + 1` elements where `N` is the size of the signal and `M` is the size of the kernel.
+*/
+public func convolution(signal: [Double], _ kernel: [Double]) -> [Double] {
+    precondition(signal.count >= kernel.count, "The signal should have at least as many elements as the kernel")
+
+    let kernelLast = UnsafePointer<Double>(kernel) + kernel.count - 1
+    let resultSize = signal.count - kernel.count + 1
+    var result = [Double](count: resultSize, repeatedValue: 0.0)
+    vDSP_convD(signal, 1, kernelLast, -1, &result, 1, vDSP_Length(resultSize), vDSP_Length(kernel.count))
+    return result
+}
+
+/**
+    Correlation between two vectors. The first vector should have at least as many elements as the second. The result
+    will have `N - M + 1` elements where `N` is the size of the first vector and `M` is the size of the second.
+*/
+public func correlation(x: [Double], _ y: [Double]) -> [Double] {
+    precondition(x.count >= y.count, "The first vector should have at least as many elements as the second")
+
+    let resultSize = x.count - y.count + 1
+    var result = [Double](count: resultSize, repeatedValue: 0.0)
+    vDSP_convD(x, 1, y, 1, &result, 1, vDSP_Length(resultSize), vDSP_Length(y.count))
+    return result
+}
+
+/**
+    Autocorrelation function.
+*/
+public func autocorrelation(x: [Double]) -> [Double] {
+    let signalSize = 3 * x.count - 2
+    var signal = [Double](count: signalSize, repeatedValue: 0.0)
+    for i in 0..<x.count {
+        signal[x.count + i - 1] = x[i]
+    }
+    return correlation(signal, x)
+}
