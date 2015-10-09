b53f1288ae766ed6906e6eb52b5af1a1d256febd
Aidan Gomez <agomez@venturemedia.com>
2015-10-09 16:01:47 -0700

    Simplify vector multiplication

--- Source/Arithmetic.swift
+++ Source/Arithmetic.swift
@@ -173,30 +173,6 @@ public func add(x: [Complex], _ y: [Complex]) -> [Complex] {
     return results
 }
 
-// MARK: Multiply
-
-public func mul(x: [Float], _ y: [Float]) -> [Float] {
-    var results = [Float](count: x.count, repeatedValue: 0.0)
-    vDSP_vmul(x, 1, y, 1, &results, 1, vDSP_Length(x.count))
-
-    return results
-}
-
-public func mul(x: [Double], _ y: [Double]) -> [Double] {
-    var results = [Double](count: x.count, repeatedValue: 0.0)
-    vDSP_vmulD(x, 1, y, 1, &results, 1, vDSP_Length(x.count))
-
-    return results
-}
-
-public func mul(x: [Complex], _ y: [Double]) -> [Complex] {
-    let results = [Complex](count: x.count, repeatedValue: Complex())
-    vDSP_vmulD(UnsafePointer<Double>(x), 2, y, 1, UnsafeMutablePointer<Double>(results), 2, vDSP_Length(x.count))
-    vDSP_vmulD(UnsafePointer<Double>(x) + 1, 2, y, 1, UnsafeMutablePointer<Double>(results) + 1, 2, vDSP_Length(x.count))
-
-    return results
-}
-
 // MARK: Divide
 
 public func div(x: [Float], _ y: [Float]) -> [Float] {
@@ -336,23 +312,58 @@ public func / (lhs: [Complex], rhs: Double) -> [Complex] {
 }
 
 public func * (lhs: [Float], rhs: [Float]) -> [Float] {
-    return mul(lhs, rhs)
+    var results = [Float](count: lhs.count, repeatedValue: 0.0)
+    vDSP_vmul(lhs, 1, rhs, 1, &results, 1, vDSP_Length(lhs.count))
+
+    return results
 }
 
 public func * (lhs: [Double], rhs: [Double]) -> [Double] {
-    return mul(lhs, rhs)
+    var results = [Double](count: lhs.count, repeatedValue: 0.0)
+    vDSP_vmulD(lhs, 1, rhs, 1, &results, 1, vDSP_Length(lhs.count))
+
+    return results
 }
 
-public func * (lhs: [Float], rhs: Float) -> [Float] {
-    return mul(lhs, [Float](count: lhs.count, repeatedValue: rhs))
+public func * (lhs: [Complex], rhs: [Double]) -> [Complex] {
+    let results = [Complex](count: lhs.count, repeatedValue: Complex())
+    vDSP_vmulD(UnsafePointer<Double>(lhs), 2, rhs, 1, UnsafeMutablePointer<Double>(results), 2, vDSP_Length(lhs.count))
+    vDSP_vmulD(UnsafePointer<Double>(lhs) + 1, 2, rhs, 1, UnsafeMutablePointer<Double>(results) + 1, 2, vDSP_Length(lhs.count))
+
+    return results
 }
 
-public func * (lhs: [Double], rhs: Double) -> [Double] {
-    return mul(lhs, [Double](count: lhs.count, repeatedValue: rhs))
+public func * (lhs: [Double], rhs: [Complex]) -> [Complex] {
+    return rhs * lhs
 }
 
-public func * (lhs: [Complex], rhs: Double) -> [Complex] {
-    return mul(lhs, [Double](count: lhs.count, repeatedValue: rhs))
+public func * (lhs: [Float], var rhs: Float) -> [Float] {
+    var result = [Float](count: lhs.count, repeatedValue: 0.0)
+    vDSP_vsmul(lhs, 1, &rhs, &result, 1, vDSP_Length(lhs.count))
+
+    return result
+}
+
+public func * (lhs: Float, rhs: [Float]) -> [Float] {
+    return rhs * lhs
+}
+
+public func * (lhs: [Double], var rhs: Double) -> [Double] {
+    var result = [Double](count: lhs.count, repeatedValue: 0.0)
+    vDSP_vsmulD(lhs, 1, &rhs, &result, 1, vDSP_Length(lhs.count))
+
+    return result
+}
+
+public func * (lhs: Double, rhs: [Double]) -> [Double] {
+    return rhs * lhs
+}
+
+public func * (lhs: [Complex], var rhs: Double) -> [Complex] {
+    var result = [Complex](count: lhs.count, repeatedValue: Complex())
+    vDSP_vsmulD(UnsafePointer<Double>(lhs), 1, &rhs, &(result[0].real), 1, vDSP_Length(lhs.count * 2))
+
+    return result
 }
 
 public func % (lhs: [Float], rhs: [Float]) -> [Float] {
