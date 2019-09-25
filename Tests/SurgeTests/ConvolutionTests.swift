1f27f54153338e9026b812d152fbc2909deb0042
Vincent Esche <regexident@gmail.com>
2019-09-25 10:21:58 +0200

    Bumped copyright from ‘… - 2018’ to ‘… - 2019’

--- Tests/SurgeTests/ConvolutionTests.swift
+++ Tests/SurgeTests/ConvolutionTests.swift
@@ -1,4 +1,4 @@
-// Copyright © 2014-2018 the Surge contributors
+// Copyright © 2014-2019 the Surge contributors
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
